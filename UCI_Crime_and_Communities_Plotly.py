b3e46ef395a6967be51b833500f79be5c22a7f22
bassantelmokadem <100968293+bassantelmokadem@users.noreply.github.com>
2024-06-05 19:19:04 +0300

    Update UCI_Crime_and_Communities_Plotly.py

--- UCI_Crime_and_Communities_Plotly.py
+++ UCI_Crime_and_Communities_Plotly.py
@@ -71,7 +71,7 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000 ))
     app.run_server(host='0.0.0.0', port=port)
 # # Reading Data
 
