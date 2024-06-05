0800cdf0f62a77d6b1725f424da018e405f0cf5e
bassantelmokadem <100968293+bassantelmokadem@users.noreply.github.com>
2024-06-05 03:03:01 +0300

    Update UCI_Crime_and_Communities_Plotly.py

--- UCI_Crime_and_Communities_Plotly.py
+++ UCI_Crime_and_Communities_Plotly.py
@@ -54,9 +54,9 @@ install('plotly')
 
 
 import dash
-import dash_core_components as dcc
+from dash import dcc
+from dash import html
 import dash_bootstrap_components as dbc
-import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import json
